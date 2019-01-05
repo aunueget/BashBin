352342864b2eb5e5d22ff559b72d540ef5d847e2
Andrew McKnight <mcknightaa@gmail.com>
2019-01-05 17:32:46 -0500

    initial commit

---
+++ pullImage.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+echo "hello world"
+
+dir="img2017"              # Remove all dots.
+
+for f in *.jpg                 # no need to use ls.
+do
+    filename=${f##*/}          # Use the last part of a path.
+    extension=${f##*.}         # Remove up to the last dot.
+    filename=${filename%.*}    # Remove from the last dot.
+    echo "$filename $dir"
+	
+	img_date=${filename:8:8}
+	img_time=${filename:16:4}
+	echo $img_date
+	echo $img_time
+	test_date=$(date +%u -d $img_date )
+	echo $test_date
+	if [[ $test_date -eq 5 ]] ; then
+		echo 'Its friday'
+		if [[ $((10#$img_time)) -gt 1800 ]] ; then
+			echo "Friday Carnite image"
+			if [[ -d $dir ]]; then     # If the directory exists
+				mv "$filename.$extension" "$dir"/ # Move file there.
+			fi
+		fi
+	elif [[ $test_date -eq 6 ]]; then
+		echo "its Saturday"
+		if [[ $((10#$img_time)) -lt 300 ]] ; then
+			echo "Saturday CarMorning image"
+			if [[ -d $dir ]]; then     # If the directory exists
+				mv "$filename.$extension" "$dir"/ # Move file there.
+			fi
+		fi
+	else
+		echo "not carnite"
+	fi
+
+done
